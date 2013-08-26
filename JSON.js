e445e0ea6f7e71d3d26abe297b469c9a31daa73f
Lastangel <kingpin2086@fullsail.edu>
2013-08-26 19:57:51 -0400

    Create JSON.js

---
+++ JSON.js
@@ -0,0 +1,39 @@
+// Clifton Abraham
+// project 4 VFW   1307
+// Date: 08/26/13
+
+var json = {
+ "Info 1": {
+    "groups":    ["Group:" , "Home"],
+    "fname":    ["First Name:" , "Clifton"],
+    "lname":    ["Last Name:" , "Abraham"],
+    "pword":    ["Password:" , "lastangel86"],
+    "email":    ["Email:" , "kingpin2086@fullsail.edu"],
+  "comments":   ["comments:" , "also clean SUV"],
+  "vaccum":     ["Vaccum:" , "Yes"],
+  "sweep":      ["Sweep:" , "Yes"],
+  "wDishes":    ["Wash dishes:" , "No"],
+  "mop":        ["Mop:" , "No"],
+  "store":      ["Store:" , "No"],
+  "everyday":   ["Everyday", "Yes"],
+  "location":   ["Location:" , "LivingRoom"]
+ },
+ "Info 2": {
+   "groups":    ["Group:" , "Home"],
+    "fname":    ["First Name:" , "Cat-Tyna"],
+    "lname":    ["Last Name:" , "Abraham"],
+    "pword":    ["Password:" , "kityn111210"],
+    "email":    ["Email:" , "aygivuponya@hotmail.com"],
+  "comments":   ["comments:" , "also clean attic"],
+  "vaccum":     ["Vaccum:" , "No"],
+  "sweep":      ["Sweep:" , "Yes"],
+  "wDishes":    ["Wash dishes:" , "Yes"],
+  "mop":        ["Mop:" , "No"],
+  "store":      ["Store:" , "No"],
+   "everyday":   ["Everyday", "No"],
+  "location":   ["Location" , "Kitchen"]
+ }
+
+
+} // end of JSON data 
+
